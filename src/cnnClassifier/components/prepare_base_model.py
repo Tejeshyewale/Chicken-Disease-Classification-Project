77936b7efe04ac6cf512523a1b732304fe116d26
Tejesh Yewale <tejeshyewale917@gmail.com>
2026-03-03 18:28:40 +0530

    Final clean structure and removed virtual environment

--- src/cnnClassifier/components/prepare_base_model.py
+++ src/cnnClassifier/components/prepare_base_model.py
@@ -1,37 +1,40 @@
-import os
-import urllib.request as request
-from zipfile import ZipFile
 import tensorflow as tf
 from pathlib import Path
 from cnnClassifier.entity.config_entity import PrepareBaseModelConfig
+
 
 class PrepareBaseModel:
     def __init__(self, config: PrepareBaseModelConfig):
         self.config = config
 
-
-    
+    # ------------------------------------------------
     def get_base_model(self):
-        self.model = tf.keras.applications.vgg16.VGG16(
+        self.model = tf.keras.applications.VGG16(
             input_shape=self.config.params_image_size,
             weights=self.config.params_weights,
             include_top=self.config.params_include_top
         )
 
-        self.save_model(path=self.config.base_model_path, model=self.model)
+        self.save_model(
+            path=self.config.base_model_path,
+            model=self.model
+        )
 
-
-    
+    # ------------------------------------------------
     @staticmethod
     def _prepare_full_model(model, classes, freeze_all, freeze_till, learning_rate):
+
+        # ✅ FIXED FREEZE LOGIC
         if freeze_all:
             for layer in model.layers:
-                model.trainable = False
+                layer.trainable = False
         elif (freeze_till is not None) and (freeze_till > 0):
             for layer in model.layers[:-freeze_till]:
-                model.trainable = False
+                layer.trainable = False
 
+        # Add custom head
         flatten_in = tf.keras.layers.Flatten()(model.output)
+
         prediction = tf.keras.layers.Dense(
             units=classes,
             activation="softmax"
@@ -49,10 +52,12 @@ class PrepareBaseModel:
         )
 
         full_model.summary()
-        return full_model
-    
 
+        return full_model
+
+    # ------------------------------------------------
     def update_base_model(self):
+
         self.full_model = self._prepare_full_model(
             model=self.model,
             classes=self.config.params_classes,
@@ -61,12 +66,12 @@ class PrepareBaseModel:
             learning_rate=self.config.params_learning_rate
         )
 
-        self.save_model(path=self.config.updated_base_model_path, model=self.full_model)
+        self.save_model(
+            path=self.config.updated_base_model_path,
+            model=self.full_model
+        )
 
-    
+    # ------------------------------------------------
     @staticmethod
     def save_model(path: Path, model: tf.keras.Model):
         model.save(path)
-
-    
-
